76dab2273274a9c2cdec9af1a06104c3031068fa
Bevin <bevinbenet@gmail.com>
2025-09-14 14:41:47 +0530

    added the matomo code

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 
+
 function App() {
   useEffect(() => {
+    // Matomo tracking code
+    var _paq = window._paq = window._paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u = "//localhost/matomo/";
+      _paq.push(['setTrackerUrl', u + 'matomo.php']);
+      _paq.push(['setSiteId', '1']);
+      var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+      g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+    })();
+
     Aos.init({
       once: true,
     });
   }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
